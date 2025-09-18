c29bb6c0d8eaac59912a655d26a5de44657a7772
secondseven <72446201+secondseven@users.noreply.github.com>
2025-09-18 11:13:58 +0100

    First pass at WA prototype for team discussion

---
+++ app/routes.js
@@ -0,0 +1,48 @@
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
+
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+// Add your routes here
+router.post('/intention-answer', function(request, response) {
+
+	var intention = request.session.data['intention']
+	if (intention == "know-site"){
+		response.redirect("/location")
+	} else {
+		response.redirect("/volume")
+	}
+})
+
+router.post('/location-selection-answer', function(request, response) {
+
+	var location_selection = request.session.data['location-selection']
+	if (location_selection == "grid-ref"){
+		response.redirect("/grid-ref")
+	} else {
+		response.redirect("/map")
+	}
+})
+
+router.post('/do-you-know-volume-answer', function(request, response) {
+
+	var do_you_know_volume = request.session.data['do-you-know-volume']
+	if (do_you_know_volume == "yes"){
+		response.redirect("/volume")
+	} else {
+		response.redirect("/??")
+	}
+})
+
+router.post('/volume-method-answer', function(request, response) {
+
+	var volume_method = request.session.data['volume-method']
+	if (volume_method == "range"){
+		response.redirect("/volume-range")
+	} else {
+		response.redirect("/volume-exact")
+	}
+})
