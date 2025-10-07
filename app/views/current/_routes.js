31671d38c56f8b32c4fb95d72dfb3c056b0fba99
chris-barrett-ddts <chris.barrett@defra.gov.uk>
2025-10-07 11:41:57 +0100

    Routing for versions

--- app/views/current/_routes.js
+++ app/views/current/_routes.js
@@ -3,118 +3,108 @@ const router = govukPrototypeKit.requests.setupRouter()
 // Add your routes here - above the module.exports line
 const folder = "/current/"
 
+
 //Resetting session data for UR
-router.get('/ur', function(request, response) {
-request.session.data = {}
-response.render(folder+'ur')
-})
-
-//Doing versioning for set variables page
-router.get('/set-variables', function(request, response) {
-response.render(folder+'set-variables')
-})
-
-router.post('/set-variables', function(request, response) {
-response.redirect(folder+'start')
-})
-
-//Doing versioning for start page
-router.get(folder+'/start', function(request, response) {
-response.render(folder+'start')
-})
-
-//Doing versioning for intention page
-router.get('/intention', function(request, response) {
-response.render(folder+'intention')
-})
-
-//Doing versioning for location page
-router.get('/location', function(request, response) {
-response.render(folder+'location')
-})
-
-//Doing versioning for do you know volume page
-router.get('/do-you-know-volume', function(request, response) {
-response.render(folder+'do-you-know-volume')
-})
-
-//Doing versioning for volume page
-router.get('/volume', function(request, response) {
-response.render(folder+'volume')
-})
-
-//Doing versioning for catchment picker page
-router.get('/catchment-picker', function(request, response) {
-response.render(folder+'catchment-picker')
-})
-
-//Doing versioning for results page
-router.get('/results', function(request, response) {
-response.render(folder+'results')
-})
-
-//Doing versioning for detailed results page
-router.get('/detailed-results', function(request, response) {
-response.render(folder+'detailed-results')
+router.get(folder + 'ur', function (request, response) {
+	request.session.data = {}
+	response.render(folder + 'ur')
 })
 
 
-router.post('/intention-answer', function(request, response) {
+
+//Set variables page
+router.get(folder + 'set-variables', function (request, response) {
+	response.render(folder + 'set-variables')
+})
+
+router.post('/set-variables', function (request, response) {
+	response.redirect(folder + 'start')
+})
+
+
+
+//Start page
+router.get(folder + 'start', function (request, response) {
+	response.render(folder + 'start')
+})
+
+router.post('/start', function (request, response) {
+	response.redirect(folder + 'intention')
+})
+
+
+//Intention page - looking for water in a location or trying to find a location
+router.get(folder + 'intention', function (request, response) {
+	response.render(folder + 'intention')
+})
+
+//if they know the site go to grid ref page if not skip grid ref and go to volume
+router.post('/intention', function (request, response) {
 	var intention = request.session.data['intention']
-	if (intention == "know-site"){
-		response.redirect("/location")
-	} else if (intention == "need-site"){
-		response.redirect("/volume")
+	if (intention == "know-site") {
+		response.redirect(folder + "location")
+	} else if (intention == "need-site") {
+		response.redirect(folder + "volume")
 	}
 })
 
-/* router.post('/location-selection-answer', function(request, response) {
 
-	var location_selection = request.session.data['location-selection']
-	if (location_selection == "grid-ref"){
-		response.redirect("/grid-ref")
-	} else {
-		response.redirect("/map")
-	}
-}) */
+//Location page
+router.get(folder + 'location', function (request, response) {
+	response.render(folder + 'location')
+})
 
-router.post('/do-you-know-volume-answer', function(request, response) {
+router.post('/location', function (request, response) {
+	response.redirect(folder + 'do-you-know-volume')
+})
+
+
+//Do you know volume page
+router.get(folder + 'do-you-know-volume', function (request, response) {
+	response.render(folder + 'do-you-know-volume')
+})
+//if they know the volume then enter it, if not go straight to results
+router.post('/do-you-know-volume', function (request, response) {
 	var do_you_know_volume = request.session.data['do-you-know-volume']
-	if (do_you_know_volume == "yes"){
-		response.redirect("/volume")
+	if (do_you_know_volume == "yes") {
+		response.redirect(folder + "volume")
 	} else {
-		response.redirect("/results")
+		response.redirect(folder + "results")
 	}
 })
 
-router.post('/volume', function(request, response) {
-	var intention = request.session.data['intention']
-	if (intention == "know-site"){
-		response.redirect("/results")
-	} else {
-		response.redirect("/catchment-picker")
-	}
+
+//Volume page
+router.get(folder + 'volume', function (request, response) {
+	response.render(folder + 'volume')
 })
 
-/* router.post('/outcome', function(request, response) {
-	var availability = request.session.data['availability']
-	if (availability == "no"){
-		response.redirect("/no-results")
-	} else {
-		response.redirect("/results")
-	}
-}) */
-
-/* router.post('/volume-method-answer', function(request, response) {
-
-	var volume_method = request.session.data['volume-method']
-	if (volume_method == "range"){
-		response.redirect("/volume-range")
-	} else {
-		response.redirect("/volume-exact")
-	}
-}) */
+router.post('/volume', function (request, response) {
+	response.redirect(folder + 'results')
+})
 
 
-    
+//Catchment picker page
+router.get(folder + 'catchment-picker', function (request, response) {
+	response.render(folder + 'catchment-picker')
+})
+
+router.post('/catchment-picker', function (request, response) {
+	response.redirect(folder + 'results')
+})
+
+
+//Results page
+router.get(folder + 'results', function (request, response) {
+	response.render(folder + 'results')
+})
+
+
+//Detailed results page
+router.get(folder + 'detailed-results', function (request, response) {
+	response.render(folder + 'detailed-results')
+})
+
+
+
 module.exports = router
