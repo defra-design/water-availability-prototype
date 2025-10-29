9798f269b13c03c719e2f5ee1b977bcb5a77519e
secondseven <72446201+secondseven@users.noreply.github.com>
2025-10-29 13:44:22 +0000

    created round 3

---
+++ app/views/sprint-2/_routes.js
@@ -0,0 +1,141 @@
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+// Add your routes here - above the module.exports line
+const folder = "/sprint-2/"
+
+
+//Resetting session data for UR
+router.get(folder + 'ur', function (request, response) {
+	request.session.data = {}
+	response.render(folder + 'ur')
+})
+
+
+
+//Set variables page
+router.get(folder + 'set-variables', function (request, response) {
+	response.render(folder + 'set-variables')
+})
+
+router.post('/set-variables', function (request, response) {
+	response.redirect(folder + 'start')
+})
+
+
+
+//Start page
+router.get(folder + 'start', function (request, response) {
+
+   if (request.session.data.participant == 1) {
+request.session.data.catchment = "Cam & Ely Ouse"
+request.session.data.waterBody = "River Wissey"
+request.session.data.aP = "Upper River Wissey (Northwold)"
+   }
+  
+   else if (request.session.data.participant == 2) {
+request.session.data.catchment = "Ribble, Douglas & Crossens"
+request.session.data.waterBody = "River Ribble"
+request.session.data.aP = "Lower Ribble"
+   }
+
+   else if (request.session.data.participant == 3) {
+request.session.data.catchment = "Witham"
+request.session.data.waterBody = "River Witham"
+request.session.data.aP = "Hykeham Bridge"
+   }
+
+   else if (request.session.data.participant == 4) {
+request.session.data.catchment = "South & West Somerset"
+request.session.data.waterBody = "River Axe"
+request.session.data.aP = "Lower River Axe at Diamond Farm"
+   }
+
+   else if (request.session.data.participant == 5) {
+request.session.data.catchment = "Wharfe & Lower Ouse"
+request.session.data.waterBody = "River Ouse (tidal Lower Ouse)"
+request.session.data.aP = "Tadcaster"
+   }
+   
+	response.render(folder + 'start')
+})
+
+router.post('/start', function (request, response) {
+	response.redirect(folder + 'location')
+})
+
+
+//Intention page - looking for water in a location or trying to find a location
+/* router.get(folder + 'intention', function (request, response) {
+	response.render(folder + 'intention')
+}) */
+
+//if they know the site go to grid ref page if not skip grid ref and go to volume
+/* router.post('/intention', function (request, response) {
+	var intention = request.session.data['intention']
+	if (intention == "know-site") {
+		response.redirect(folder + "location")
+	} else if (intention == "need-site") {
+		response.redirect(folder + "volume")
+	}
+}) */
+
+
+//Location page
+router.get(folder + 'location', function (request, response) {
+	response.render(folder + 'location')
+})
+
+router.post('/location', function (request, response) {
+	response.redirect(folder + 'detailed-results')
+})
+
+
+//Do you know volume page
+/* router.get(folder + 'do-you-know-volume', function (request, response) {
+	response.render(folder + 'do-you-know-volume')
+})
+//if they know the volume then enter it, if not go straight to results
+router.post('/do-you-know-volume', function (request, response) {
+	var do_you_know_volume = request.session.data['do-you-know-volume']
+	if (do_you_know_volume == "yes") {
+		response.redirect(folder + "volume")
+	} else {
+		response.redirect(folder + "results")
+	}
+}) */
+
+
+//Volume page
+/* router.get(folder + 'volume', function (request, response) {
+	response.render(folder + 'volume')
+})
+
+router.post('/volume', function (request, response) {
+	response.redirect(folder + 'results')
+}) */
+
+
+//Catchment picker page
+/* router.get(folder + 'catchment-picker', function (request, response) {
+	response.render(folder + 'catchment-picker')
+})
+
+router.post('/catchment-picker', function (request, response) {
+	response.redirect(folder + 'results')
+}) */
+
+
+//Results page
+/* router.get(folder + 'results', function (request, response) {
+	response.render(folder + 'results')
+}) */
+
+
+//Detailed results page
+router.get(folder + 'detailed-results', function (request, response) {
+	response.render(folder + 'detailed-results')
+})
+
+
+
+module.exports = router
