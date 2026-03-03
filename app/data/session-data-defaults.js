fb21cc795a320c25c30a3eb9894a4fc1ad61e2be
secondseven <72446201+secondseven@users.noreply.github.com>
2026-03-03 16:44:13 +0000

    slightly tidied up detailed results for non consumptive

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -6,7 +6,7 @@ module.exports = {
 "watertype" : "",
 
 "summerAvail": [
-  {
+[ {
   "month": "march",
   "daysAvail": 14,
   "daysUnavail": 17,
@@ -40,13 +40,118 @@ module.exports = {
   "month": "september",
   "daysAvail": 10,
   "daysUnavail": 20,
+},],
+ [ {
+  "month": "2march",
+  "daysAvail": 14,
+  "daysUnavail": 17,
 },
+ {
+  "month": "2april",
+  "daysAvail": 23,
+  "daysUnavail": 7,
+},
+ {
+  "month": "may",
+  "daysAvail": 19,
+  "daysUnavail": 12,
+},
+ {
+  "month": "june",
+  "daysAvail": 8,
+  "daysUnavail": 22,
+},
+ {
+  "month": "july",
+  "daysAvail": 6,
+  "daysUnavail": 25,
+},
+ {
+  "month": "august",
+  "daysAvail": 9,
+  "daysUnavail": 22,
+},
+ {
+  "month": "september",
+  "daysAvail": 10,
+  "daysUnavail": 20,
+},],
+[ {
+  "month": "3march",
+  "daysAvail": 14,
+  "daysUnavail": 17,
+},
+ {
+  "month": "2april",
+  "daysAvail": 23,
+  "daysUnavail": 7,
+},
+ {
+  "month": "may",
+  "daysAvail": 19,
+  "daysUnavail": 12,
+},
+ {
+  "month": "june",
+  "daysAvail": 8,
+  "daysUnavail": 22,
+},
+ {
+  "month": "july",
+  "daysAvail": 6,
+  "daysUnavail": 25,
+},
+ {
+  "month": "august",
+  "daysAvail": 9,
+  "daysUnavail": 22,
+},
+ {
+  "month": "september",
+  "daysAvail": 10,
+  "daysUnavail": 20,
+},],
+[ {
+  "month": "4march",
+  "daysAvail": 14,
+  "daysUnavail": 17,
+},
+ {
+  "month": "4april",
+  "daysAvail": 23,
+  "daysUnavail": 7,
+},
+ {
+  "month": "may",
+  "daysAvail": 19,
+  "daysUnavail": 12,
+},
+ {
+  "month": "june",
+  "daysAvail": 8,
+  "daysUnavail": 22,
+},
+ {
+  "month": "july",
+  "daysAvail": 6,
+  "daysUnavail": 25,
+},
+ {
+  "month": "august",
+  "daysAvail": 9,
+  "daysUnavail": 22,
+},
+ {
+  "month": "september",
+  "daysAvail": 10,
+  "daysUnavail": 20,
+},],
 ],
 
 
 "winterAvail": [
-  {
-  "month": "october",
+  [{
+  "month": "1october",
   "daysAvail": 20,
   "daysUnavail": 10,
 },
@@ -69,7 +174,82 @@ module.exports = {
   "month": "february",
   "daysAvail": 15,
   "daysUnavail": 13,
-}
+},],
+[ {
+  "month": "2october",
+  "daysAvail": 20,
+  "daysUnavail": 10,
+},
+ {
+  "month": "november",
+  "daysAvail": 28,
+  "daysUnavail": 7,
+},
+ {
+  "month": "december",
+  "daysAvail": 25,
+  "daysUnavail": 6,
+},
+ {
+  "month": "january",
+  "daysAvail": 19,
+  "daysUnavail": 11,
+},
+ {
+  "month": "february",
+  "daysAvail": 15,
+  "daysUnavail": 13,
+}],
+[ {
+  "month": "3october",
+  "daysAvail": 20,
+  "daysUnavail": 10,
+},
+ {
+  "month": "november",
+  "daysAvail": 28,
+  "daysUnavail": 7,
+},
+ {
+  "month": "december",
+  "daysAvail": 25,
+  "daysUnavail": 6,
+},
+ {
+  "month": "january",
+  "daysAvail": 19,
+  "daysUnavail": 11,
+},
+ {
+  "month": "february",
+  "daysAvail": 15,
+  "daysUnavail": 13,
+}],
+[ {
+  "month": "4october",
+  "daysAvail": 20,
+  "daysUnavail": 10,
+},
+ {
+  "month": "november",
+  "daysAvail": 28,
+  "daysUnavail": 7,
+},
+ {
+  "month": "december",
+  "daysAvail": 25,
+  "daysUnavail": 6,
+},
+ {
+  "month": "january",
+  "daysAvail": 19,
+  "daysUnavail": 11,
+},
+ {
+  "month": "february",
+  "daysAvail": 15,
+  "daysUnavail": 13,
+}]
 ]
 
 
