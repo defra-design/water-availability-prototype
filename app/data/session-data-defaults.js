743e67abde3ff86a908d2419fea1c168dd4eba9c
secondseven <72446201+secondseven@users.noreply.github.com>
2026-03-03 12:12:05 +0000

    half working dynamic month percentages

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -3,8 +3,45 @@ module.exports = {
   // Insert values here
 "riverCatchmentData":[],
 "groundwaterCatchmentData":[],
-"watertype" : ""
+"watertype" : "",
 
+"summerAvail": [
+  {
+  "month": "march",
+  "daysAvail": 5,
+  "daysUnavail": 26,
+},
+ {
+  "month": "april",
+  "daysAvail": 23,
+  "daysUnavail": 7,
+},
+ {
+  "month": "may",
+  "daysAvail": 25,
+  "daysUnavail": 6,
+},
+ {
+  "month": "june",
+  "daysAvail": 19,
+  "daysUnavail": 11,
+},
+ {
+  "month": "july",
+  "daysAvail": 12,
+  "daysUnavail": 19,
+},
+ {
+  "month": "august",
+  "daysAvail": 5,
+  "daysUnavail": 26,
+},
+ {
+  "month": "september",
+  "daysAvail": 5,
+  "daysUnavail": 25,
+},
+]
 
 }
 
--- app/filters.js
+++ app/filters.js
@@ -8,3 +8,28 @@ const addFilter = govukPrototypeKit.views.addFilter
 
 // Add your filters here
 
+addFilter('percentAvail', function(x,daysAvail, daysUnavail) {
+    x= ((daysAvail/(daysAvail+daysUnavail))*100);
+    return x;
+});
+
+addFilter('availColour', function(x) {
+    if (x<=33) {
+        x="govuk-tag--red"
+    }
+    else if (x>66) {
+        x="govuk-tag--green"
+    }
+    else {
+        x="govuk-tag--yellow"
+    }
+    return x;
+});
+
+addFilter('PercentAvailAgg', function(x) {
+    for(months of x) {
+        month
+
+    };
+    return x;
+});
